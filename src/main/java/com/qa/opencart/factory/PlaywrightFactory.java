48528a01cdc8dae679e13e4f2a571e3dc3433ced
Evgeny_Sorokin4 <sorokinevgen21@gmail.com>
2023-04-05 14:38:48 +0300

    Initial commit

---
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory.java
@@ -0,0 +1,125 @@
+package com.qa.opencart.factory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Base64;
+import java.util.Properties;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class PlaywrightFactory {
+
+	Properties prop;
+	
+	private static final ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+	private static final ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+	private static final ThreadLocal<Page> tlPage = new ThreadLocal<>();
+	private static final ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+	
+	public static Playwright getPlaywright() {
+		return tlPlaywright.get();
+	}
+	public static Browser getBrowser() {
+		return tlBrowser.get();
+	}
+	public static BrowserContext getBrowserContext() {
+		return tlBrowserContext.get();
+	}
+
+	public static Page getPage() {
+		return tlPage.get();
+	}
+	
+	
+	
+	public Page initBrowser(Properties prop) {
+		
+		String browserName = prop.getProperty("browser").trim();
+		boolean headless = prop.getProperty("headless").equalsIgnoreCase("true");
+		System.out.println("browser name is : " + browserName);
+		
+		//playwright = Playwright.create();
+		tlPlaywright.set(Playwright.create());
+		
+		switch (browserName.toLowerCase()) {
+		case "chromium":
+			//browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(headless)));
+			break;
+			
+		case "firefox":
+			//browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			tlBrowser.set(getPlaywright().firefox().launch(new BrowserType.LaunchOptions().setHeadless(headless)));
+			break;
+			
+		case "safari":
+			//browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			tlBrowser.set(getPlaywright().webkit().launch(new BrowserType.LaunchOptions().setHeadless(headless)));
+			break;
+			
+		case "chrome":
+			//browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+			tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(headless)));
+			break;
+			
+		case "edge":
+			//browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("edge").setHeadless(false));
+			tlBrowser.set(getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setChannel("msedge").setHeadless(headless)));
+			break;
+
+		default:
+			System.out.println("Please pass the right browser name.... ");
+			break;
+		}
+		
+//		browserContext = browser.newContext();
+		tlBrowserContext.set(getBrowser().newContext());
+//		page = browserContext.newPage();
+		tlPage.set(getBrowserContext().newPage());
+//		
+//		page.navigate(prop.getProperty("url").trim());
+		getPage().navigate(prop.getProperty("url").trim());
+		
+		
+		
+		return getPage();
+		
+	}
+	
+	
+	/*
+	 * this method is used to initialize the properties from config file
+	 */
+	public Properties init_prop() {
+		
+		try {
+			FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
+			prop = new Properties();
+			prop.load(ip);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return prop;
+		
+	}
+	
+	/*
+	 * Take Screenshot
+	 */
+	public static String takeScreenshot() {
+		String path = System.getProperty("user.dir") + "/screenshot/" + System.currentTimeMillis() + ".png";
+		
+		byte[] buffer = getPage().screenshot(new Page.ScreenshotOptions()
+				.setPath(Paths.get(path))
+				.setFullPage(true));
+
+		return Base64.getEncoder().encodeToString(buffer);
+	}
+}
---
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -0,0 +1,49 @@
+package com.qa.opencart.pages;
+
+import com.microsoft.playwright.Page;
+
+public class LoginPage {
+
+	
+	private final Page page;
+
+	//1.String Locators - OR
+	private final String emailId = "//input[@id='input-email']";
+	private final String password = "//input[@id='input-password']";
+	private final String loginBtn = "//input[@value='Login']";
+	private final String forgotPwdLink = "//div[@class='form-group']//a[normalize-space()='Forgotten Password']";
+	private final String logoutLink = "//a[@class='list-group-item'][normalize-space()='Logout']";
+	
+	//2.page constructor
+	public LoginPage(Page page) {
+
+		this.page = page;
+	}
+	
+	
+	//3.Page actions/methods
+	public String getLoginPageTitle() {
+
+		return page.title();
+	}
+	
+	public boolean isForgotPwdLinkExist() {
+
+		return page.isVisible(forgotPwdLink);
+	}
+	
+	public boolean doLogin(String appUserName, String appPassword) {
+		System.out.println("App creds: " + appUserName + ":" + appPassword);
+		page.fill(emailId, appUserName);
+		page.fill(password, appPassword);
+		page.click(loginBtn);
+		if(page.isVisible(logoutLink)) {
+			System.out.println("user is logged is successfuly....");
+			return true;
+		}else {
+			System.out.println("user is not logged in successfully....");
+			return false;
+		}
+		
+	}
+}
